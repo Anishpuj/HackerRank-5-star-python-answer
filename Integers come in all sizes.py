04c62ab72db8fa278b480ebe812b9a8c7f8d3ac5
Anish Pujari <anishputtya46@gmail.com>
2023-10-24 19:39:01 +0530

    Create Integers come in all sizes.py

---
+++ Integers come in all sizes.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Integers Come In All Sizes in Python - Hacker Rank Solution
+# Python 3
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Integers Come In All Sizes in Python - Hacker Rank Solution START
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+print((A**B)+(C**D))
+# Integers Come In All Sizes in Python - Hacker Rank Solution END
