5ef7744de589ef23d59749fc6e8098cfd9633957
Anish Pujari <anishputtya46@gmail.com>
2023-10-24 19:25:31 +0530

    Create itertools.permutations.py

---
+++ itertools.permutations.py
@@ -0,0 +1,12 @@
+# itertools.permutations() in Python - Hacker Rank Solution
+# Python 3
+# itertools.permutations() in Python - Hacker Rank Solution START
+from itertools import permutations
+
+s,k = input().split()
+
+words = list(permutations(s,int(k)))
+words = sorted(words, reverse=False)
+for word in words:
+    print(*word,sep='')
+# itertools.permutations() in Python - Hacker Rank Solution END
